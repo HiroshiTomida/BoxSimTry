1029db5bc29b4586ba720b7cefcb91f3512b3edc
kana takezawa <kana.takezawa@mail.toyota-td.jp>
2026-07-21 14:27:10 +0900

    特定のjsonファイルを読み込んで新しい中身を作る

--- box_test.py
+++ box_test.py
@@ -1,5 +1,6 @@
 import zipfile
 import os
+import json
 
 zip_path = r"C:\Users\ttdcuser\Desktop\BOX_test\uploaad\750_20260707_003010.zip"
 extract_dir = "extracted_files"
@@ -7,7 +8,22 @@ extract_dir = "extracted_files"
 if not os.path.exists(extract_dir):
     os.makedirs(extract_dir)
 
+# zipファイル解凍
 with zipfile.ZipFile(zip_path, "r") as zip_ref:
     zip_ref.extractall(extract_dir)
 
 print("解凍が完了しました。")
+
+# jsonファイルを読み込む
+with open(r"C:\usr\takezawa\BoxSimTry\extracted_files\summary.json", encoding="utf-8") as f:
+     data = json.load(f)
+print(data)
+
+# 新しい中身を作成する
+data_new= {
+    "file_id": data["file_id"],
+    "original_file_name": data["original_file_name"],
+    "box_url": "url",
+    "updated_datetime": "time"
+}
+print(data_new)
